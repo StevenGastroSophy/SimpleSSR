8e43c537c3cc639caed9e9e90846cb26e763e8bf
StevenGastroSophy <kaspersky518@gmail.com>
2020-06-22 17:57:42 +0800

    Update darkMarkdown

---
+++ src/app/components/MainHeader.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class MainHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleTurnPage = (pathname) => {
+    const {
+      history,
+    } = this.props;
+
+    history.push({ pathname });
+  }
+
+  render() { 
+    return (
+      <div className="container mainHeader">
+        <div className="row">
+          <div onClick={() => {this.handleTurnPage("/");}} className="col-xs-4 text-center">Home</div>
+          <div onClick={() => {this.handleTurnPage("/article/art20200611001");}} className="col-xs-4 text-center">SSR</div>
+          <div onClick={() => {this.handleTurnPage("/about");}} className="col-xs-4 text-center">About</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainHeader;
