e2e04346b4b26ac8f7301daa59dbe78118357479
StevenGastroSophy <kaspersky518@gmail.com>
2020-06-23 17:56:05 +0800

    Update darkMarkdown

--- src/app/components/Marked.js
+++ src/app/components/Marked.js
@@ -2,7 +2,7 @@ import React from 'react';
 import marked from 'marked';
 
 const Marked = ({ text }) => (
-  <div dangerouslySetInnerHTML={{ __html: marked(text) }} className="marked" />
+  <div dangerouslySetInnerHTML={{ __html: marked(text) }} className="marked darkMarkdown" />
 );
 
 export default Marked;
